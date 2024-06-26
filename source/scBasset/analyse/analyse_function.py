2d1244590839578821b5cace60a457376d0afc14
pascalo <pascal.oberbach@gmail.com>
2024-06-26 15:56:28 +0200

    add comment

--- source/scBasset/analyse/analyse_function.py
+++ source/scBasset/analyse/analyse_function.py
@@ -105,71 +105,133 @@ def load_model(ad, trained_model):
     model.load_weights(trained_model)
     return model
 
-# def intercept_depth(ad, model, type_data):
-#     intercept = get_intercept(model) # get_intercept function
-#     sc.pp.filter_cells(ad, min_counts=0)
 
-#     f, ax = plt.subplots(figsize=(4,4))
-#     r = scipy.stats.pearsonr(intercept, np.log10(ad.obs['n_genes']))[0]
-#     sns.scatterplot(x=intercept, y=np.log10(ad.obs['n_genes']), ax=ax)
-#     ax.set_xlabel('intercept')
-#     ax.set_ylabel('log10(n_peaks)')
-#     ax.set_title(f'{type_data}_Pearson R: %.3f'%r)
+def embedding(model, results_path):
+    """
+        Extract the weights of the final dense layer
 
-def embedding(model, type_data, results_path):
+        Parameters:
+        - model: trained model
+        - results_path: path to save the results
+
+        Output:
+        - CSV projection file
+        
+    """
+
     proj = get_cell_embedding(model) # get_cell_embedding function
-    pd.DataFrame(proj).to_csv(f'{results_path}/projection_cage_{type_data}.csv')
+    pd.DataFrame(proj).to_csv(f'{results_path}/projection_cage.csv')
 
-def plotting_embedding(ad_file, color_by, type_data, results_path, option = False):
+
+def plotting_embedding(ad_file, color_by, results_path):
     """
-    Input: ad_file
-    Output: embedding of the 'color_by'
+        Visualize the embedding of the last dense layer weights, colored by a specified attribute
+
+        Parameters:
+        - ad_file: file with samples and features
+        - color_by: column name to color by
+        - results_path: path where the PNG will be saved
+
+        Output:
+        - PNG plot of the embeddings 
     """
-    f, ax = plt.subplots(figsize=(4, 4))
-    if option:
-        projection_data = pd.read_csv(f'{results_path}/projection_cage_{type_data}.csv', index_col=0, nrows=72)
-        ad_file.obsm['projection'] = projection_data.values
-    else:
-        ad_file.obsm['projection'] = pd.read_csv(f'{results_path}/projection_cage_{type_data}.csv', index_col=0).values
+
+    # Load the projection
+    projection_path = f'{results_path}/projection_cage.csv'
+    ad_file.obsm['projection'] = pd.read_csv(projection_path, index_col=0).values
+    
+    # Compute neighbors and UMAP
     sc.pp.neighbors(ad_file, use_rep='projection')
     sc.tl.umap(ad_file)
     sc.tl.leiden(ad_file)
-    sc.pl.umap(ad_file, color=color_by, ax=ax, size=350, title = f'{type_data} embedding')
+    
+    # Create a figure and axis
+    f, ax = plt.subplots(figsize=(10, 10))
+    
+    # Plot UMAP embedding, disable immediate display
+    sc.pl.umap(ad_file, color=color_by, ax=ax, size=350, title='embedding', show=False)
+    
+    # Adjust the layout
+    plt.tight_layout()
+
+    output_file = f'{results_path}/embedding_{color_by}.png'
+    
+    # Save the plot and handle potential exceptions
+    try:
+        plt.savefig(output_file, format='png')
+        print(f"Embedding plot saved to {output_file}")
+    except Exception as e:
+        print(f"Failed to save embedding plot: {e}")
+
+    plt.close()
+    
 
 def add_patient_info(ad):
     """
-    Input: anndata file
-    Output: anndata file with patient and cell line information
+        Add patient or cell line information to every sample in the AnnData file
+
+        Parameters:
+        - ad: AnnData file with samples and features
+
+        Output:
+        - Updated AnnData file with patient and cell line information
     """
+
     ad.obs['Patient'] = np.where(ad.obs.index.str.startswith('s'), 'patient', 'cell_line')
     ad.obs['Patient'] = ad.obs['Patient'].astype('category')
 
 def hormone_classe(row):
     """
-    Input: row of hormone csv
-    Output: return the hormone class
+        Add hormone status information to every sample corresponding to a patient in the AnnData file
+
+        Parameters:
+        - row: single row of the AnnData file containing sample information
+
+        Output:
+        - Subgroup name of the breast cancer type
     """
-    if row['ER+_HER2-'] == 1:
-        return 'ER+_HER2-'
-    elif row['HER2+'] == 1:
-        return 'HER2+'
-    elif row['TNBC'] == 1:
-        return 'TNBC'
+    
+        #Luminal A
+    if row['Oestrogen'] == 1 and row['progesteron'] == 1 and row['her2'] == 0:
+        return 'Luminal A'
+    # Luminal B(Her2+)
+    elif row['Oestrogen'] == 1 and row['progesteron'] == 1 and row['her2'] == 1:
+        return 'Luminal B (HER2+)'
+    elif row['Oestrogen'] == 1 and row['progesteron'] == 0 and row['her2'] == 1:
+        return 'Luminal B (HER2+)'
+    # Luminal B(Her2-)
+    elif row['Oestrogen'] == 1 and row['progesteron'] == 0 and row['her2'] == 0:
+        return 'Luminal B (HER2-)'
+    elif row['Oestrogen'] == 0 and row['progesteron'] == 1 and row['her2'] == 0:
+        return 'Luminal B (HER2-)'
+
+    # Her2-enriched
+    elif row['Oestrogen'] == 0 and row['progesteron'] == 0 and row['her2'] == 1:
+        return 'Her2-enriched'
+    # Basal-like
+    elif row['Oestrogen'] == 0 and row['progesteron'] == 0 and row['her2'] == 0:
+        return 'Basal-like'
 
 def add_hormone_info(ad, hormone_path):
     """
-    Input: anndata file
-    Output: anndata file with hormone information 
+        Add hormone status information to every sample corresponding to a patient in the AnnData file
+
+        Parameters:
+        - ad: AnnData file with samples and features 
+
+        Output:
+        - Updated AnnData file with hormonal status information for every patient
     """
-    hormone = pd.read_csv(hormone_path, sep = ' ', header = 0)
+
+    hormone = pd.read_csv(hormone_path, sep = '\t', header = 0)
     hormone['Classe'] = hormone.apply(hormone_classe, axis=1)
     sample_class_map = dict(zip(hormone['Sample_id'], hormone['Classe']))
     ad.obs['Hormone'] = [sample_class_map.get(sample_id, None) for sample_id in ad.obs.index]
 
 
-#############################
-######### Overlapp ##########
-#############################
+################################
+######### Overlapping ##########
+################################
 
 
 def adjust_bed_position(start, end, seq_len = 1344):
@@ -296,6 +358,8 @@ def calculate_overlap_percentage(bed_file):
 
         Output:
         - Percentage of overlap in the BED file
+        - global overlapping
+        - overlapping per chromosomes
     """
 
     a = pybedtools.BedTool(bed_file)
